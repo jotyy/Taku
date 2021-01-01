9a3d6cbf99d62479939bf8065418bb7760463edf
Jotyy <838373812@qq.com>
2021-01-01 21:43:24 +0800

    feat: create record dao

--- lib/data/local/record/dao/record.dao.dart
+++ lib/data/local/record/dao/record.dao.dart
@@ -8,4 +8,10 @@ part 'record.dao.g.dart';
 @UseDao(tables: [Records])
 class RecordDao extends DatabaseAccessor<AppDatabase> with _$RecordDaoMixin {
   RecordDao(AppDatabase db) : super(db);
+
+  Future<List<Record>> getRecords() => select(records).get();
+
+  Future insertRecord(RecordsCompanion record) => into(records).insert(record);
+
+  Future deleteRecord(int id) => deleteRecord(id);
 }
